8ce316d9ee502f3880a73f949f31bcb6e6f0c55a
Stéphane TOUBIANA <info@stephane-toubiana.fr>
2015-12-07 19:51:29 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # backbone.crossfilter
 A Crossfilter port to Backbone Framework.
+
+
+// instanciation du plugin CrossFilter
+var myBCfilter = new backbone.crossfilter({
+	config: {
+		attributs: [ "attribute1", "attribute2", "attribute3" ],
+	}
+});
+
+Here you instantiate the filter with a configuration JSON which has an array "attributs" with the attributes you want to look at.
+Then, you instantiate the filtered collection with the method myBCfilter.getFilterCollection() wich return a Backbone collection,
+and you can do what you want with.
+For example, you can turn on the backbone comparator with one of your attributes and pass it to a view.
+
+var filteredCollection = myBCfilter.getFilterCollection();
+filteredCollection.comparator = 'attribute3';
+var myView = new View({collection: filteredCollection});
+
+// instanciation vues filtres
+Now you have set your filter and have a reference to the filter output "filteredCollection", you have access to backbone collections of 
+criterias with the method myBCfilter.access("attribute").
+It gives you a collection of all the choices possible within the category you want to look at.
+You will pass these collections to their views that you had already set up.
+
+var auteursVue    = new CriteriaView({collection: myBCfilter.access("attribute1")}),
+	genresVue     = new CriteriaView({collection: myBCfilter.access("attribute2")}),
+	editionsVue   = new CriteriaView({collection: myBCfilter.access("attribute3")}),
+
+The Criteria model is build for you with this schema :
+
+var Criteria = Backbone.Model.extend({
+	defaults: {
+		coche: false,
+		type: "attribute1",
+		nom: "choice1",
+		nombre: "7"
+	},
+})
+
+and it has a method myCriteria.prepaF() that you must call when you get a click event in your view (here a marionette view).
+
+myCriteriaView = Marionette.ItemView.extend({
+	ui: { 'filter': 'input' },
+	events: {
+		'click @ui.filter': 'prepaF',
+	},
+	prepaF: function(){
+		this.model.prepaF();
+	},
+});
+
+if you have read this far and already tried to use this plugin, it has not worked !
+The final method you must use to complete the task is myBCfilter.buildFilter(myCollection) to pass the collection 
+you want to filter to your backbone.crossfilter instance. I do it in the fetch''s success callback to be sure to 
+have the entire collection in my filter.
+
+myBaseCollection.fetch({
+	success: function(collection){
+		myBCfilter.buildFilter(collection);
+	},
+});
+
+Now you gonna kick this collection'' ass and filter it up your desires.
+If you keep your Backbone or Marionette views tight linked to your collections/models, whenever a criteria is clicked 
+your views are rendered automatically but maybe you want to update the data in your filtered collection manually.
+This is possible with the method myBCfilter.updateFilter().
+
+For convenience, you have access to the IDs of the filtered models in the base collection with the method myBCfilter.getSortedIDs().
+Like the name told it, they are sorted.
+
+Enjoy ! \o/
---
+++ backbone.crossfilter.js
@@ -0,0 +1,173 @@
+define([
+	'backbone',
+	'underscore',
+	'application',
+
+	'crossfilter',
+], function (Backbone, _, app) {
+	'use strict';
+
+return Backbone.Model.extend({
+	defaults: {
+		dimension: {},
+		groupe: {},
+		filtresActifs: {},
+		critereCollection: {},
+	},
+	initialize: function (opts){
+		var moduleCF = this;
+
+		this.set('criteria', 
+			Backbone.Model.extend({
+				defaults: {
+					coche: false,
+					type: "NA",
+					nom: "NA",
+					nombre: "NA"
+				},
+				prepaF: function(){
+					if(this.get('coche')){
+						this.set('coche', false);
+					} else {
+						this.set('coche', true);
+					}
+
+					var data = {
+						filtreType: this.get('type'),
+						filtreValeur: this.get('nom'),
+						filtreCoche: this.get('coche'),
+					};
+					moduleCF.setFiltre(data);
+				}
+			})
+		);
+
+		var CriteriaGroup = Backbone.Collection.extend({
+				model: this.get('criteria'),
+			}),
+			LivresFiltres = Backbone.Collection.extend();
+
+		this.set('filteredCollection', new LivresFiltres() );
+		this.set('config', opts.config);
+		_.each(this.get('config').attributs, function (att){
+			moduleCF.get('critereCollection')[att] = new CriteriaGroup();
+		});
+
+		app.vent.on('livre:clic', function (donnees){
+			moduleCF.clicFiltre(donnees);
+		});
+		console.log('this ' , this);
+	},
+	buildFilter: function (collec){
+		var moduleCF = this,
+			Criteria = this.get('criteria');
+
+		// creation base de données crossfilter
+		this.set('baseCollection', collec);
+		this.set('superfiltre', crossfilter(
+			_.map(collec.models, function (model){
+				return model.attributes;
+			})
+		));
+
+		//creation dimensions et groupes crossfilter
+		_.each(moduleCF.get('config').attributs, function (att){
+			moduleCF.get('dimension')[att] = moduleCF.get('superfiltre').dimension(function (d) { return d[att]; });
+			moduleCF.get('groupe')[att] = moduleCF.get('dimension')[att].group().all();
+			moduleCF.get('filtresActifs')[att] = [];
+		});
+
+		// Collections de criteres possibles traduits en Marionette
+		_.each(moduleCF.get('groupe'), function (grp, grpName){
+			_.each(grp, function (crit){
+				moduleCF.get('critereCollection')[grpName].add(new Criteria({
+					type: grpName,
+					nom: crit.key,
+					nombre: crit.value
+				}));
+			});
+		});
+		this.updateFilter();
+	},
+	updateFilter: function (){
+		this.set( 'donneesFiltrees', this.get('dimension')[this.get('config').attributs[0]].top(Infinity) );
+
+		var moduleCF = this,
+			filterBuffer = [],
+			IDs = this.getSortedIDs();
+
+		_.each(IDs, function (id){
+			filterBuffer.push(moduleCF.get('baseCollection').get(id));
+		});
+		this.get('filteredCollection').set(filterBuffer);
+
+		//met à jour les sommes de critères
+		_.each(moduleCF.get('critereCollection'), function (collec){
+			_.each(collec.models, function (mod){
+				var monCritere = collec.get(mod.cid);
+				_.each(moduleCF.get('groupe')[monCritere.get('type')], function (crit){
+					if (crit.key === monCritere.get('nom')) {
+						monCritere.set('nombre', crit.value);
+					}
+				});
+			});
+		});
+	},
+	setFiltre: function (data){
+		var moduleCF = this;
+		var config = moduleCF.get('config').attributs;
+
+		if(data.filtreCoche){
+			moduleCF.get('filtresActifs')[data.filtreType].push(data.filtreValeur);
+		} else {
+			var pool_filtre = moduleCF.get('filtresActifs')[data.filtreType];
+			var x = pool_filtre.indexOf(data.filtreValeur);
+			pool_filtre.splice(x, 1);
+		}
+
+		_.each(moduleCF.get('dimension'), function (dim, key){
+			if(moduleCF.get('filtresActifs')[key][0]){
+				moduleCF.FFhelper(
+					moduleCF.get('dimension')[key],
+					moduleCF.get('filtresActifs')[key]
+				);
+			} else {
+				moduleCF.get('dimension')[key].filterAll();
+			}
+		});
+
+		this.updateFilter();
+	},
+	FFhelper: function (maDim, monFA){
+		maDim.filterFunction(function (critere_possible){
+			if(monFA.indexOf(critere_possible) >= 0){
+				return critere_possible;
+			}
+		});
+	},
+	clicFiltre: function (donnees){
+		var moduleCF = this;
+
+		_.each(this.get('config').attributs, function (att){
+			if (donnees.type === att){
+				var critere = moduleCF.get('critereCollection')[att].findWhere({nom: donnees.nom_critere_clique});
+				critere.prepaF();
+			}
+		});
+	},
+	access: function (attribut){
+		return this.get('critereCollection')[attribut];
+	},
+	getFilterCollection: function (){
+		return this.get('filteredCollection');
+	},
+	getSortedIDs: function (){
+		return _.pluck(this.get('donneesFiltrees'), 'id').sort();
+	},
+});
+});
+
+
+
+// filtre sur attribut unique contenant des tags multiples
+// sortie de l'inverse
